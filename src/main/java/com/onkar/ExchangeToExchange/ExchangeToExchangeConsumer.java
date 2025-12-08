c67f3d081bafdfd8d79707472c52a9dbd0716d08
onkar oulkar <onkaroulkar@gmail.com>
2025-12-08 22:50:45 +0530

    third commit

---
+++ src/main/java/com/onkar/ExchangeToExchange/ExchangeToExchangeConsumer.java
@@ -0,0 +1,24 @@
+package com.onkar.ExchangeToExchange;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ExchangeToExchangeConsumer {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        DeliverCallback deliverCallback = (consumerTag, delivery)->{
+            String message = new String(delivery.getBody());
+            System.out.println("message received: "+message);
+        };
+        channel.basicConsume("mobile",true,deliverCallback,consumerTag->{});
+    }
+}
